c57df9ae5d700304a86a489fc90a7dc03c4258e5
TheUnicDoudz <theunicdoudz@gmail.com>
2025-02-18 22:04:04 +0100

    Integrate EventSub to receive message from a specific Channel

--- twitchapi/chatbot.py
+++ twitchapi/chatbot.py
@@ -1,30 +1,37 @@
 import json
 import logging
 import os
+import secrets
 from datetime import datetime
 from enum import Enum
+from random import randint
+
 import requests
 from requests import Response
 
-from twitchapi.auth import UriServer, REDIRECT_URI, DEFAULT_TIMEOUT
+from twitchapi.auth import AuthServer, REDIRECT_URI_AUTH, DEFAULT_TIMEOUT
 from twitchapi.exception import TwitchAuthentificationError, TwitchEndpointError, TwitchMessageNotSentWarning
+from twitchapi.eventsub import EventSub, REDIRECT_URI_MESSAGE
 
 
 class TwitchEndpoint(Enum):
     TWITCH_ENDPOINT = "https://api.twitch.tv/helix/"
     USER_ID = "users?login="
     SEND_MESSAGE = "chat/messages"
+    EVENTSUB_SUBSCRIPTION = "eventsub/subscriptions"
 
 
 class ChatBot:
     ACCESS_TOKEN_FILE = ".access_token"
 
-    def __init__(self, client_id, client_secret, bot_name, channel_name, redirect_uri=REDIRECT_URI,
+    def __init__(self, client_id: str, client_secret: str, bot_name: str, channel_name: str,
+                 redirect_uri_auth: str = REDIRECT_URI_AUTH, redirect_uri_message: str = REDIRECT_URI_MESSAGE,
                  timeout=DEFAULT_TIMEOUT):
         self._client_id = client_id
         self.__client_secret = client_secret
 
-        self.__uri = UriServer()
+        self.__uri = AuthServer()
+        self.__event_sub = EventSub()
         self.__credentials = None
 
         if not os.path.exists(self.ACCESS_TOKEN_FILE):
@@ -33,7 +40,7 @@ class ChatBot:
                                                                                    scope=["user:read:chat",
                                                                                           "user:write:chat",
                                                                                           "user:bot", "channel:bot"],
-                                                                                   redirect_uri=redirect_uri,
+                                                                                   redirect_uri=redirect_uri_auth,
                                                                                    timeout=timeout)
             self.__credentials = {"access_token": access_token, "refresh_token": refresh_token,
                                   "expire_date": expire_date}
@@ -63,8 +70,10 @@ class ChatBot:
         self._channel_id = self._get_id(channel_name)
         logging.debug("Channel id: " + self._channel_id)
 
+        self.__connect_eventsub_message()
+
     def _get_id(self, user_name: str) -> str:
-        data = self.__get_request(endpoint= TwitchEndpoint.USER_ID.value + user_name)
+        data = self.__get_request(endpoint=TwitchEndpoint.USER_ID.value + user_name)
         return data['data'][0]['id']
 
     def send_message(self, message: str, reply_message_id: str = None):
@@ -76,13 +85,40 @@ class ChatBot:
         if reply_message_id:
             data["reply_parent_message_id"] = reply_message_id
 
-        add_data = self.__post_request(endpoint= TwitchEndpoint.SEND_MESSAGE.value, data=data)['data'][0]
+        add_data = self.__post_request(endpoint=TwitchEndpoint.SEND_MESSAGE.value, data=data)['data'][0]
 
         if not add_data["is_sent"]:
             logging.warning(f"Message not sent: {add_data['drop_reason']}")
             drop_code = add_data["drop_reason"]["code"]
             drop_message = add_data["drop_reason"]["message"]
             raise TwitchMessageNotSentWarning(f"{drop_code}: {drop_message}")
+
+    def __connect_eventsub_message(self, callback: str = REDIRECT_URI_MESSAGE):
+        condition = {
+            "type": "channel.chat.message",
+            "version": "1",
+            "condition": {
+                "broadcaster_user_id": self._channel_id,
+                "user_id": self._bot_id
+            }
+        }
+
+        response = self.__connect_evensub(condition)
+
+    def __connect_evensub(self, condition:dict, callback: str = REDIRECT_URI_MESSAGE):
+        secret_length = randint(16, 32)
+        secret = secrets.token_urlsafe(secret_length)
+        data = {
+            "transport":{
+                "method": "webhook",
+                "callback": callback,
+                "secret": secret
+            }
+        }
+        data.update(condition)
+
+        add_data = self.__post_request(TwitchEndpoint.EVENTSUB_SUBSCRIPTION.value, data=data)
+        return add_data
 
     def __get_request(self, endpoint: str) -> dict:
         url_endpoint = TwitchEndpoint.TWITCH_ENDPOINT.value + endpoint
@@ -123,3 +159,6 @@ class ChatBot:
             "Client-Id": self._client_id,
             "Content-Type": "application/json"
         }
+
+    def stop_eventsub(self):
+        self.__event_sub.stop()
