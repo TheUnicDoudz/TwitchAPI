aa49f3ccf234a48efa1bb43cf7540a422e7b250f
TheUnicDoudz <theunicdoudz@gmail.com>
2025-02-19 16:20:38 +0100

    All endpoint used are stored in a common file

---
+++ twitchapi/utils.py
@@ -0,0 +1,8 @@
+
+class TwitchEndpoint:
+    TWITCH_ENDPOINT = "https://api.twitch.tv/helix/"
+    USER_ID = "users?login="
+    SEND_MESSAGE = "chat/messages"
+    EVENTSUB_SUBSCRIPTION = "eventsub/subscriptions"
+    TWITCH_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws"
+    TWITCH_AUTH_URL = "https://id.twitch.tv/oauth2/token"
