843c61bc4c6030659ec2026ea8015cbba308ccb8
TheUnicDoudz <theunicdoudz@gmail.com>
2025-02-20 18:25:31 +0100

    Get message from the database

--- twitchapi/eventsub.py
+++ twitchapi/eventsub.py
@@ -1,14 +1,19 @@
 import logging
 import sqlite3
-from datetime import datetime
+import time
+from collections.abc import Callable
+from queue import Queue
+from typing import Any
 
+from idna.idnadata import scripts
 from websocket import WebSocketApp
 
 from twitchapi.auth import AuthServer
-from twitchapi.utils import TwitchEndpoint
+from twitchapi.utils import TwitchEndpoint, ThreadWithExc
+from twitchapi.exception import KillThreadException
 import json
 import os
-from threading import Thread, Lock
+from threading import Lock
 
 
 class EventSub(WebSocketApp):
@@ -16,16 +21,24 @@ class EventSub(WebSocketApp):
     def __init__(self, bot_id: str, channel_id: str, subscription_types: list[str], auth_server: AuthServer):
         super().__init__(url=TwitchEndpoint.TWITCH_WEBSOCKET_URL, on_message=self.on_message, on_open=self.on_open,
                          on_close=self.on_close, on_error=self.on_error)
+
         self.__session_id = None
         self.__auth = auth_server
         self._bot_id = bot_id
         self._channel_id = channel_id
         self._subscription_types = subscription_types
+        self._last_messages = Queue(maxsize=100)
 
-        self._last_message_date = None
         self.__db = sqlite3.connect(database=os.path.dirname(__file__) + "/database/TwitchDB", check_same_thread=False)
         self.__cursor = self.__db.cursor()
-        self.__lock = Lock()
+
+        self.__lock_db = Lock()
+
+        self.__thread = ThreadWithExc(target=self.__execute_script_db)
+        self.__thread.start()
+
+        self.__first_id_message_session = ""
+
 
     def on_message(self, ws, message):
         logging.debug("Message received:" + message)
@@ -36,7 +49,6 @@ class EventSub(WebSocketApp):
 
         message_type = metadata["message_type"]
         msg_timestamp = metadata["message_timestamp"][:-4]
-        self._last_message_date = datetime.strptime(msg_timestamp, "%Y-%m-%dT%H:%M:%S.%f")
 
         match message_type:
             case "session_welcome":
@@ -48,29 +60,38 @@ class EventSub(WebSocketApp):
                             logging.info("Subscription to get chat message")
                             condition = {"broadcaster_user_id": self._channel_id, "user_id": self._bot_id}
                             self.__subscription(subscription_type=subscription, condition=condition)
+
             case "notification":
                 logging.info("Receive notification message")
                 subscription_type = payload["subscription"]["type"]
                 match subscription_type:
                     case "channel.chat.message":
                         logging.info("Process a message")
-                        event = payload["event"]
-                        id = event['message_id']
-                        user_name = event["chatter_user_login"]
-                        msg = event["message"]["text"]
-                        date = msg_timestamp
-                        cheer= True if event["cheer"] else False
-                        emote= True if len(event["message"]["fragments"]) > 1 else False
-                        thread_id = event["reply"]['thread_message_id'] if event["reply"] else None
-                        parent_id = event["reply"]['parent_message_id'] if event["reply"] else None
-                        self.__db__insert__message(id=id, user=user_name, message=msg, date=date, parent_id=parent_id,
-                                                   thread_id=thread_id, cheer=cheer, emote=emote)
+                        self.__process_message(payload=payload, date=msg_timestamp)
+
+    def __process_message(self, payload:dict[str, Any], date):
+
+        event = payload["event"]
+        id = event['message_id']
+        if not self.__first_id_message_session:
+            self.__first_id_message_session = id
+
+        user_name = event["chatter_user_login"]
+        message = event["message"]["text"]
+        cheer = True if event["cheer"] else False
+        emote = True if len(event["message"]["fragments"]) > 1 else False
+        thread_id = event["reply"]['thread_message_id'] if event["reply"] else None
+        parent_id = event["reply"]['parent_message_id'] if event["reply"] else None
+        self.__db__insert__message(id=id, user=user_name, message=message, date=date,
+                                   parent_id=parent_id, thread_id=thread_id, cheer=cheer, emote=emote)
 
     def on_error(self, ws, message):
         logging.error(message)
 
     def on_close(self, ws, close_status_code, close_msg):
         logging.info("Close websocket")
+        self.__thread.raise_exc(KillThreadException)
+        self.__thread.join()
 
     def on_open(self, ws):
         logging.info(f"Connect to {TwitchEndpoint.TWITCH_WEBSOCKET_URL}")
@@ -90,7 +111,7 @@ class EventSub(WebSocketApp):
 
     def __db__insert__message(self, id: str, user: str, message: str, date: str, parent_id: str = None,
                               thread_id: str = None, cheer: bool = False, emote: bool = False):
-        date_str = f"date('{date}')"
+        date_str = f"DATETIME('{date}', 'subsec')"
         if parent_id:
             if not thread_id:
                 raise ValueError(f"Missing thread_id value for parent_id {thread_id}")
@@ -102,8 +123,36 @@ class EventSub(WebSocketApp):
         logging.info("Insert message data in database")
         logging.debug(script)
         try:
-            self.__cursor.execute(script)
-            self.__db.commit()
+            self.__lock_method(self.__cursor.execute, self.__lock_db, script)
             logging.info('Data ingested')
         except Exception as e:
             logging.error(str(e.__class__) + ": " + str(e))
+
+    def __execute_script_db(self):
+        try:
+            while True:
+                logging.info("Try commiting ingested data...")
+                self.__lock_method(self.__db.commit, self.__lock_db)
+                logging.info("Data commited!")
+                time.sleep(10)
+        except KillThreadException:
+            if self.__lock_db.locked():
+                self.__lock_db.release()
+            logging.info("Commit last change on database")
+            self.__lock_method(self.__db.commit, self.__lock_db)
+            logging.info("Stop data ingestion")
+
+    def __lock_method(self, callback:Callable, lock:Lock, *args, **kwargs):
+        lock.acquire()
+        data = callback(*args, **kwargs)
+        lock.release()
+        return data
+
+    def get_last_message(self, start_id:str = None):
+        if not start_id:
+            start_id = self.__first_id_message_session
+        script = f"""SELECT * from message
+                              WHERE date > (SELECT date FROM message WHERE id='{start_id}')"""
+        recs = self.__lock_method(self.__cursor.execute, self.__lock_db, script)
+        return recs
+
