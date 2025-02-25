f762fc3974d9d9180707a5175d1832a1c592bc47
TheUnicDoudz <theunicdoudz@gmail.com>
2025-02-25 16:20:57 +0100

    Add Twitch subscription model

--- twitchapi/utils.py
+++ twitchapi/utils.py
@@ -2,18 +2,207 @@ import threading
 import inspect
 import ctypes
 from collections.abc import Callable
+from ctypes.wintypes import PRECT
 
 
 class TwitchEndpoint:
     TWITCH_ENDPOINT = "https://api.twitch.tv/helix/"
-    USER_ID = "users?login="
-    SEND_MESSAGE = "chat/messages"
-    EVENTSUB_SUBSCRIPTION = "eventsub/subscriptions"
     TWITCH_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws"
     TWITCH_AUTH_URL = "https://id.twitch.tv/oauth2/token"
 
+    USER_ID = "users?login="
+    SEND_MESSAGE = "chat/messages"
+    EVENTSUB_SUBSCRIPTION = "eventsub/subscriptions"
+
+
+class TwitchSubscriptionType:
+    MESSAGE = "channel.chat.message"
+
+    SUBSCRIBE = "channel.subscribe"
+    SUBGIFT = "channel.subscription.gift"
+    RESUB_MESSAGE = "channel.subscription.message"
+
+    RAID = "channel.raid"
+
+    CHANNEL_POINT_ACTION = "channel.channel_points_custom_reward_redemption.add"
+
+    POLL_BEGIN = "channel.poll.begin"
+    POLL_END = "channel.poll.end"
+
+    PREDICTION_BEGIN = "channel.prediction.begin"
+    PREDICTION_LOCK = "channel.prediction.lock"
+    PREDICTION_END = "channel.prediction.end"
+
+    VIP_ADD = "channel.vip.add"
+
+    STREAM_ONLINE = "stream.online"
+    STREAM_OFFLINE = "stream.offline"
+
+    BITS = "channel.bits.use"
+    CHEER = "channel.cheer"
+
+
+
+class TwitchSubscriptionModel:
+
+    def __init__(self, broadcaster_user_id, user_id):
+        self.MESSAGE = {
+            "right": ["user:read:chat", "user:bot", "channel:bot"],
+            "payload": {
+                "type": TwitchSubscriptionType.MESSAGE,
+                "condition": {"broadcaster_user_id": broadcaster_user_id, "user_id": user_id},
+                "version": "1"
+            }
+        }
+
+        self.SUBSCRIBE = {
+            "right": ["channel:read:subscriptions"],
+            "payload": {
+                "type": TwitchSubscriptionType.SUBSCRIBE,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.SUBGIFT = {
+            "right": ["channel:read:subscriptions"],
+            "payload": {
+                "type": TwitchSubscriptionType.SUBGIFT,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.RESUB_MESSAGE = {
+            "right": ["channel:read:subscriptions"],
+            "payload": {
+                "type": TwitchSubscriptionType.RESUB_MESSAGE,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.RAID = {
+            "right": [],
+            "payload": {
+                "type": TwitchSubscriptionType.RAID,
+                "version": "1",
+                "condition": {"to_broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.RAID_SOMEONE = {
+            "right": [],
+            "payload": {
+                "type": TwitchSubscriptionType.RAID,
+                "version": "1",
+                "condition": {"from_broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.CHANNEL_POINT_ACTION = {
+            "right": ["channel:read:redemptions", "channel:manage:redemptions"],
+            "payload": {
+                "type": TwitchSubscriptionType.CHANNEL_POINT_ACTION,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.POLL_BEGIN = {
+            "right": ["channel:read:polls", "channel:manage:polls"],
+            "payload": {
+                "type": TwitchSubscriptionType.POLL_BEGIN,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.POLL_END = {
+            "right": ["channel:read:polls", "channel:manage:polls"],
+            "payload": {
+                "type": TwitchSubscriptionType.POLL_END,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.PREDICTION_BEGIN = {
+            "right": ["channel:read:predictions", "channel:manage:predictions"],
+            "payload": {
+                "type": TwitchSubscriptionType.PREDICTION_BEGIN,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.PREDICTION_LOCK = {
+            "right": ["channel:read:predictions", "channel:manage:predictions"],
+            "payload": {
+                "type": TwitchSubscriptionType.PREDICTION_LOCK,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.PREDICTION_END = {
+            "right": ["channel:read:predictions", "channel:manage:predictions"],
+            "payload": {
+                "type": TwitchSubscriptionType.PREDICTION_END,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.VIP_ADD = {
+            "right": ["channel:read:vips", "channel:manage:vips"],
+            "payload": {
+                "type": TwitchSubscriptionType.VIP_ADD,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.STREAM_ONLINE = {
+            "right": [],
+            "payload": {
+                "type": TwitchSubscriptionType.STREAM_ONLINE,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.STREAM_OFFLINE = {
+            "right": [],
+            "payload": {
+                "type": TwitchSubscriptionType.STREAM_OFFLINE,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.BITS = {
+            "right": ["bits:read"],
+            "payload": {
+                "type": TwitchSubscriptionType.BITS,
+                "version": "beta",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+        self.CHEER = {
+            "right": ["bits:read"],
+            "payload": {
+                "type": TwitchSubscriptionType.CHEER,
+                "version": "1",
+                "condition": {"broadcaster_user_id": broadcaster_user_id}
+            }
+        }
+
+
 class TriggerSignal:
     MESSAGE = "message"
+
 
 def _async_raise(tid, exctype):
     '''Raises an exception in the threads with id tid'''
@@ -29,10 +218,12 @@ def _async_raise(tid, exctype):
         ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), None)
         raise SystemError("PyThreadState_SetAsyncExc failed")
 
+
 class ThreadWithExc(threading.Thread):
     '''A thread class that supports raising an exception in the thread from
        another thread.
     '''
+
     def _get_my_tid(self):
         """determines this (self's) thread id
 
@@ -40,7 +231,7 @@ class ThreadWithExc(threading.Thread):
         thread, to get the identity of the thread represented by this
         instance.
         """
-        if not self.is_alive(): # Note: self.isAlive() on older version of Python
+        if not self.is_alive():  # Note: self.isAlive() on older version of Python
             raise threading.ThreadError("the thread is not active")
 
         # do we have it cached?
@@ -80,14 +271,15 @@ class ThreadWithExc(threading.Thread):
         caller thread, to raise an exception in the context of the
         thread represented by this instance.
         """
-        _async_raise( self._get_my_tid(), exctype )
+        _async_raise(self._get_my_tid(), exctype)
+
 
 class TriggerMap:
 
     def __init__(self):
         self.__callbacks = {}
 
-    def add_trigger(self, callback:Callable, trigger_value:str):
+    def add_trigger(self, callback: Callable, trigger_value: str):
         if trigger_value in self.__callbacks:
             raise KeyError(f"There's already a callback react to {trigger_value}!!")
         self.__callbacks[trigger_value] = callback
